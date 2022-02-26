5ef27010e858e0ab91722dc63b834716f87c4190
Yaifer <JHANNON@madisoncollege.edu>
2022-02-25 21:42:11 -0600

    doing git hub pages builds
    added home page property  to package.json the npm build and renamed the build folder

---
+++ docs/precache-manifest.acdde481f266a0871a2fb033a496f753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6756ef3f0731366e3d3f73404f238ad8",
+    "url": "https//Yaifer.github.io/amplify-react-app/index.html"
+  },
+  {
+    "revision": "1479bd1e5e3e414e3721",
+    "url": "https//Yaifer.github.io/amplify-react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fcd6dc6ee4cb3b71e1e0",
+    "url": "https//Yaifer.github.io/amplify-react-app/static/js/2.964a0cab.chunk.js"
+  },
+  {
+    "revision": "3eabca05c8261bdc8a8b",
+    "url": "https//Yaifer.github.io/amplify-react-app/static/js/3.dba27cfa.chunk.js"
+  },
+  {
+    "revision": "1479bd1e5e3e414e3721",
+    "url": "https//Yaifer.github.io/amplify-react-app/static/js/main.af509820.chunk.js"
+  },
+  {
+    "revision": "3229500d1fb9ccfdfc54",
+    "url": "https//Yaifer.github.io/amplify-react-app/static/js/runtime~main.ca343fb6.js"
+  }
+]);
