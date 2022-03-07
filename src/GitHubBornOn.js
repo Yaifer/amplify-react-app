db6d76d189f23f1220a49d63c12d57199624d4b7
Yaifer <JHANNON@madisoncollege.edu>
2022-03-07 15:24:47 -0600

    Bad code.
    This code has a problem

--- src/GitHubBornOn.js
+++ src/GitHubBornOn.js
@@ -1,3 +1,21 @@
+import { API } from 'aws-amplify';
+import {useEffect, useState} from 'react';
+
 export const GitHubBornOn = () => {
-    return (<h2>Yaifer - 2008-05-25T12:00:00</h2>)
+    
+    const [bornOnInfo, setBornOnInfo] = useState({login:"<login>", created_at: "<createdAt>"});
+    const fetchGitHubBornOnInfo = async () => {
+
+        try {const data = await API.get('cryptoapi', '/born' );
+        setBornOnInfo(data.bornOnInfo);
+        }
+        catch (err) {
+            setBornOnInfo({login: "Error", created_at: "Error"});
+        }
+    };
+
+    useEffect(() => fetchGitHubBornOnInfo(), []);
+    
+    return (<h2>{bornOnInfo.login} - {bornOnInfo.created_at}</h2>
+    );
 };
